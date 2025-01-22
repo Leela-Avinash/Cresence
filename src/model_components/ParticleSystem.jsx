1c77d4d2af74813efa9513a136f1bfe19a4f7269
Leela-Avinash <leelaavinash24@gmail.com>
2025-01-22 12:12:57 +0530

    made the cresence Text morph responsively

--- src/model_components/ParticleSystem.jsx
+++ src/model_components/ParticleSystem.jsx
@@ -22,12 +22,10 @@ const extractCombinedGeometry = (model) => {
         }
     });
 
-    return (
-        BufferGeometryUtils.mergeGeometries(geometries, true)
-    );
+    return BufferGeometryUtils.mergeGeometries(geometries, true);
 };
 
-const generateParticlesOnSurface = (geometry, count) => {
+const generateParticlesOnSurface = (geometry, count, scaleFactor = 1) => {
     const tempPosition = new Float32Array(count * 3);
     const tempColor = new Float32Array(count * 3);
     console.log(tempPosition);
@@ -37,13 +35,12 @@ const generateParticlesOnSurface = (geometry, count) => {
 
     for (let i = 0; i < count; i++) {
         const faceIndex = Math.floor(Math.random() * faceCount) * 3;
-        
 
         const vA = new THREE.Vector3().fromBufferAttribute(
             positionAttribute,
             faceIndex
         );
-        if(i == 0)  console.log(faceIndex, vA);
+        if (i == 0) console.log(faceIndex, vA);
         const vB = new THREE.Vector3().fromBufferAttribute(
             positionAttribute,
             faceIndex + 1
@@ -58,32 +55,31 @@ const generateParticlesOnSurface = (geometry, count) => {
         const sqrtR1 = Math.sqrt(r1);
 
         const barycentricCoords = [1 - sqrtR1, sqrtR1 * (1 - r2), sqrtR1 * r2];
-        
-
 
         const point = new THREE.Vector3()
             .addScaledVector(vA, barycentricCoords[0])
             .addScaledVector(vB, barycentricCoords[1])
             .addScaledVector(vC, barycentricCoords[2]);
-        if(i == 0)  {
+        if (i == 0) {
             console.log(faceIndex, vA, vB, vC);
             console.log(r1, r2, sqrtR1);
             console.log(barycentricCoords);
             console.log(point);
         }
+        point.multiplyScalar(scaleFactor);
 
         tempPosition[i * 3] = point.x;
         tempPosition[i * 3 + 1] = point.y;
         tempPosition[i * 3 + 2] = point.z;
 
         if (i < count * 0.2) {
-            tempColor[i * 3] = 1.0; 
+            tempColor[i * 3] = 1.0;
             tempColor[i * 3 + 1] = 0.8;
-            tempColor[i * 3 + 2] = 0.0; 
+            tempColor[i * 3 + 2] = 0.0;
         } else {
             tempColor[i * 3] = 0.6;
-            tempColor[i * 3 + 1] = 0.2; 
-            tempColor[i * 3 + 2] = 1.0; 
+            tempColor[i * 3 + 1] = 0.2;
+            tempColor[i * 3 + 2] = 1.0;
         }
     }
 
@@ -93,16 +89,16 @@ const generateParticlesOnSurface = (geometry, count) => {
 /* ------------------------------- */
 /* 🌌 Particle System Component */
 /* ------------------------------- */
-const generateParticlesOnPlane = (width, height,breadth, count) => {
+const generateParticlesOnPlane = (width, height, breadth, count) => {
     const tempPosition = new Float32Array(count * 3);
     const tempColor = new Float32Array(count * 3);
 
     for (let i = 0; i < count; i++) {
         // Randomly distribute particles on the plane
         const x = Math.random() * width - width / 2; // Center the particles on the plane
-        const y = Math.random() * height - height / 2+1;
-        const z = Math.random()*breadth-breadth/2; // Keep particles on the plane (z = 0)
-        if(i==0) console.log(x, y, z);
+        const y = Math.random() * height - height / 2 + 1;
+        const z = Math.random() * breadth - breadth / 2; // Keep particles on the plane (z = 0)
+        if (i == 0) console.log(x, y, z);
 
         tempPosition[i * 3] = x;
         tempPosition[i * 3 + 1] = y;
@@ -128,125 +124,136 @@ const ParticleSystem = () => {
     const particlesRef = useRef();
     const materialRef = useRef();
     const newModelRef = useRef();
-    const AmbientLightRef=useRef();
-    const DirectLightRef=useRef();
+    const AmbientLightRef = useRef();
+    const DirectLightRef = useRef();
+    const [Scale_c, setScale] = React.useState([1, 1, 1]);
     // const scroll = useScroll();
     const startTimeRef = useRef(null);
+    const [resizeCompleted, setResizeCompleted] = React.useState(false);
+    // const modelA = useGLTF("/fontforweb.glb");
+    const modelB = useGLTF("models/fontforweb.glb"); 
+    const { scene, nodes } = useGLTF("models/fontforweb.glb"); 
+    const meshes = [];
+    Object.values(nodes).forEach((node) => {
+        if (node.isMesh) {
+            meshes.push(node);
+        }
+    });
+    useEffect(() => {
+        const handleResize = () => {
+            const width = window.innerWidth;
 
-    // const modelA = useGLTF("/fontforweb.glb"); // Replace with your GLTF path
-    const modelB = useGLTF("models/fontforweb.glb"); // Replace with your GLTF path
-    const {scene,nodes} = useGLTF("models/fontforweb.glb"); // Replace with your GLTF path
-    // console.log(modelA) // Assuming model has multiple meshes
+            if (width <= 480) { // Small phones (e.g., iPhone SE, small Androids)
+                setScale([0.4, 0.4, 0.4]); // Very small scale for very small screens
+            } else if (width <= 600) { // Larger phones (e.g., iPhone 6/7/8, Galaxy S)
+                setScale([0.5, 0.5, 0.5]); // Slightly larger scale for small phones
+            } else if (width <= 768) { // Tablets in portrait mode (e.g., iPad, Android tablets)
+                setScale([0.6, 0.6, 0.6]); // Scale for tablets, iPads in portrait mode
+            } else if (width <= 1024) { // Larger tablets, landscape (e.g., iPad Pro, Android tablets)
+                setScale([0.66, 0.66, 0.66]); // Slightly larger scale for landscape tablet screens
+            } else if (width <= 1280) { // Small laptops (e.g., MacBook, some smaller laptops)
+                setScale([1, 1, 1]); // Default scale for small laptops
+            } else if (width <= 1440) { // Medium-sized laptops/desktops
+                setScale([1.1, 1.1, 1.1]); // Larger scale for medium laptops
+            } else if (width <= 1600) { // Large laptops/desktops
+                setScale([1.2, 1.2, 1.2]); // Slightly larger scale for large desktop screens
+            } else { // Large desktop monitors (e.g., 4K, large-screen desktop)
+                setScale([1.3, 1.3, 1.3]); // Maximum scale for large screens
+            }
+            setResizeCompleted(true); // Set resize as complete
+        };
 
-  // Accessing all meshes within the model
-  const meshes = [];
-  Object.values(nodes).forEach((node) => {
-    if (node.isMesh) {
-      meshes.push(node);
-    }
-  });
+        handleResize(); // Run it immediately for the first render
+        window.addEventListener("resize", handleResize);
 
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []); 
 
     useEffect(() => {
-        const particleCount = 50000;
+        if (!resizeCompleted) return; 
 
-        // const geometryA = extractCombinedGeometry(modelA);
+        const particleCount = 50000;
         const geometryB = extractCombinedGeometry(modelB);
-        const planeWidth = 2; // Adjust width of the plane
-        const planeHeight = 2; // Adjust height of the plane
-        const planeBreadth=2
-        const particlesA = generateParticlesOnPlane(planeWidth, planeHeight,planeBreadth, particleCount);
-        const particlesB = generateParticlesOnSurface(geometryB, particleCount);
+        const planeWidth = 2;
+        const planeHeight = 2;
+        const planeBreadth = 2;
+
+        const particlesA = generateParticlesOnPlane(planeWidth, planeHeight, planeBreadth, particleCount);
+        const particlesB = generateParticlesOnSurface(geometryB, particleCount, 1);
 
         const geometry = new THREE.BufferGeometry();
-        console.log(geometry);
-        geometry.setAttribute(
-            "position",
-            new THREE.BufferAttribute(particlesA.tempPosition, 3)
-        );
-        geometry.setAttribute(
-            "targetPosition",
-            new THREE.BufferAttribute(particlesB.tempPosition, 3)
-        );
-        geometry.setAttribute(
-            "color",
-            new THREE.BufferAttribute(particlesA.tempColor, 3)
-        );
+        geometry.setAttribute("position", new THREE.BufferAttribute(particlesA.tempPosition, 3));
+        geometry.setAttribute("targetPosition", new THREE.BufferAttribute(particlesB.tempPosition, 3));
+        geometry.setAttribute("color", new THREE.BufferAttribute(particlesA.tempColor, 3));
 
         particlesRef.current.geometry = geometry;
-        particlesRef.current.rotation.x= Math.PI/2;
-        particlesRef.current.scale.set(5,5,5);
-        newModelRef.current.rotation.x= Math.PI/2;
-        newModelRef.current.scale.set(5,5,5);
-        // newModelRef.current.material.opacity=1;
+        particlesRef.current.rotation.x = Math.PI / 2;
+        particlesRef.current.scale.set(5, 5, 5);
+        newModelRef.current.rotation.x = Math.PI / 2;
+        newModelRef.current.scale.set(5, 5, 5);
+
         newModelRef.current.traverse((child) => {
             if (child.isMesh) {
-              child.material = new THREE.MeshStandardMaterial({
-                color: 0xffffff, 
-                opacity: 0, // Initially hidden
-                transparent: true, 
-                depthWrite: false, // Ensure no depth writing
-                depthTest: false, // Ensure no depth testing
-                emissive: 0x000000,
-                roughness: 0.2,
-                metalness: 0.7,
-              });
+                child.material = new THREE.MeshStandardMaterial({
+                    color: 0xffffff,
+                    opacity: 0,
+                    transparent: true,
+                    depthWrite: false,
+                    depthTest: false,
+                    emissive: 0x000000,
+                    roughness: 0.2,
+                    metalness: 0.7,
+                });
             }
-          });
-        console.log(newModelRef.current);
+        });
 
         startTimeRef.current = performance.now();
+
         gsap.to(materialRef.current.uniforms.fadeFactor, {
             value: 0,
-            delay: 3, // Fade-out after 8 seconds
-            duration: 2, // Fade-out duration
-            ease: "power2.inOut", // Smooth easing
-          });
-        gsap.to(newModelRef.current.material,{
-            opacity:1,
-            delay:3,
-            duration:3,
+            delay: 3,
+            duration: 2,
             ease: "power2.inOut",
-        })
+        });
+        gsap.to(newModelRef.current.material, {
+            opacity: 1,
+            delay: 3,
+            duration: 3,
+            ease: "power2.inOut",
+        });
+
         if (meshes.length > 0) {
             meshes.forEach((mesh) => {
-              // GSAP animation to fade in each mesh
-              gsap.to(mesh.material, {
-                delay:2.5,
-                opacity: 1,
-                duration: 2,  // Fade duration
-                ease: "power2.inOut",
-              });
+                gsap.to(mesh.material, {
+                    delay: 2.5,
+                    opacity: 1,
+                    duration: 2,
+                    ease: "power2.inOut",
+                });
             });
-          }
-          
-        // Example: Animate the ambient light intensity using GSAP
+        }
+
         gsap.to(AmbientLightRef.current, {
-          intensity: 1, 
-          delay:3.0,       // New intensity value
-          duration: 3,         // Duration of the animation (in seconds)
-          ease: "power2.inOut", // Easing function
+            intensity: 1,
+            delay: 3.0,
+            duration: 3,
+            ease: "power2.inOut",
         });
         gsap.to(DirectLightRef.current, {
-            intensity: 0.5, 
-            delay:3.0,       // New intensity value
-            duration: 3,         // Duration of the animation (in seconds)
-            ease: "power2.inOut", // Easing function
-          });
-    
-        // Example: Animate the color of the ambient light using GSAP
-        // gsap.to(lightRef.current, {
-        //   color: "#ff5733",    // New color value (HEX format)
-        //   duration: 3,         // Duration of the color transition
-        //   ease: "power2.inOut", // Easing function
-        // });
-
-    }, [modelB]);
+            intensity: 0.5,
+            delay: 3.0,
+            duration: 3,
+            ease: "power2.inOut",
+        });
+    }, [modelB, resizeCompleted]);
 
     useFrame(() => {
         if (startTimeRef.current && materialRef.current) {
             // Calculate elapsed time
-            const elapsedTime = (performance.now() - startTimeRef.current) / 1000; // Convert ms to seconds
+            const elapsedTime =
+                (performance.now() - startTimeRef.current) / 1000; // Convert ms to seconds
 
             // Calculate progress (0 to 1, loops every 5 seconds)
             const progress = Math.min(elapsedTime / 3, 1); // Clamp progress between 0 and 1
@@ -259,7 +266,8 @@ const ParticleSystem = () => {
             // Update uniforms
             materialRef.current.uniforms.morphFactor.value = morphFactor;
             materialRef.current.uniforms.scaleFactor.value = scaleFactor;
-            materialRef.current.uniforms.dispersionFactor.value = dispersionFactor;
+            materialRef.current.uniforms.dispersionFactor.value =
+                dispersionFactor;
         }
         // if (particlesRef.current) {
         //     particlesRef.current.rotation.y += 0.001; // Adjust the rotation speed as desired
@@ -267,17 +275,17 @@ const ParticleSystem = () => {
     });
 
     return (
-        <group>
-        <points ref={particlesRef} scale={[1, 1, 1]}>
-            <shaderMaterial
-                ref={materialRef}
-                uniforms={{
-                    morphFactor: { value: 0 },
-                    scaleFactor: { value: 1 },
-                    dispersionFactor: { value: 0 },
-                    fadeFactor: { value: 1 },
-                }}
-                vertexShader={`
+        <group scale={Scale_c}>
+            <points ref={particlesRef} scale={[1, 1, 1]}>
+                <shaderMaterial
+                    ref={materialRef}
+                    uniforms={{
+                        morphFactor: { value: 0 },
+                        scaleFactor: { value: 1 },
+                        dispersionFactor: { value: 0 },
+                        fadeFactor: { value: 1 },
+                    }}
+                    vertexShader={`
           attribute vec3 targetPosition;
           attribute vec3 color;
           varying vec3 vColor;
@@ -300,7 +308,7 @@ const ParticleSystem = () => {
             gl_Position = projectionMatrix * modelViewMatrix * vec4(morphedPosition, 1.0);
           }
         `}
-                fragmentShader={`
+                    fragmentShader={`
           varying vec3 vColor;
           uniform float fadeFactor;
           void main() {
@@ -309,18 +317,26 @@ const ParticleSystem = () => {
             gl_FragColor = vec4(vColor * fadeFactor, fadeFactor); 
           }
         `}
-                transparent
+                    transparent
+                />
+            </points>
+            <ambientLight
+                ref={AmbientLightRef}
+                intensity={0}
+                color={"#925ce0"}
             />
-        </points>
-        <ambientLight ref={AmbientLightRef} intensity={0} color={'#925ce0'} />
-        <directionalLight ref={DirectLightRef}  intensity={0} position={[0, 0, 5]} />
-        <primitive
-        object={scene}
-        ref={newModelRef}
-        scale={[1, 1, 1]}
-        position={[0, 0, 0]}
-      />
-      </group>
+            <directionalLight
+                ref={DirectLightRef}
+                intensity={0}
+                position={[0, 0, 5]}
+            />
+            <primitive
+                object={scene}
+                ref={newModelRef}
+                scale={[1, 1, 1]}
+                position={[0, 0, 0]}
+            />
+        </group>
     );
 };
 
@@ -328,15 +344,11 @@ const ParticleSystem = () => {
 /* 🎥 Main Scene Component */
 /* ------------------------------- */
 const ParticleMorphing = () => {
-    
     return (
-        
         <Canvas
             camera={{ position: [0, 0, 1] }}
             style={{ height: "100vh", background: "black" }}
         >
-
-
             <ScrollControls>
                 <ParticleSystem />
                 <OrbitControls />
